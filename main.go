e7231fd4012b57f4590d38a310ec86288208c89f
Justin Thurman <justin.thurman@bevy.com>
2024-06-20 09:10:41 -0400

    Refactor to use `slog`; make telnet default option

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"log"
+	log "log/slog"
 	"net"
 	"os"
 	"ts-game/engine"
@@ -10,29 +10,28 @@ import (
 
 func main() {
 	server := engine.New()
-	if len(os.Args) == 1 {
-		fmt.Println("No mode provided, defaulting to local")
-		server.Connect(os.Stdin, os.Stdout, func() { fmt.Println("Goodbye!") })
-	} else if os.Args[1] == "local" {
-		server.Connect(os.Stdin, os.Stdout, func() { fmt.Println("Goodbye!") })
-	} else if os.Args[1] == "telnet" {
-		log.Println("Listening for TCP connections...")
+	if len(os.Args) == 1 || os.Args[1] == "telnet" {
+		log.Info("Listening for TCP connections...")
 		listener, err := net.Listen("tcp", ":8081")
 		if err != nil {
-			log.Fatal(err.Error())
+			log.Error(err.Error())
+			os.Exit(1)
 		}
 		go func() {
 			for {
 				conn, err := listener.Accept()
 				if err != nil {
-					log.Printf("Error accepting connection: %v", err.Error())
+					log.Error("Error accepting connection", "error", err.Error())
 					continue
 				}
 				server.Connect(conn, conn, func() { conn.Close() })
 			}
 		}()
+	} else if os.Args[1] == "local" {
+		server.Connect(os.Stdin, os.Stdout, func() { fmt.Println("Goodbye!") })
 	} else {
-		log.Fatalf("Unknown mode: %v", os.Args[1])
+		log.Error("Unknown mode", "mode", os.Args[1])
+		os.Exit(1)
 	}
 	server.Start()
 }
