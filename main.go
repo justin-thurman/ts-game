056437cb07e01b27dafb2987ad5e602651ab5f8c
Justin Thurman <justin.thurman@bevy.com>
2024-06-20 07:51:31 -0400

    Parse connection mode from args

--- main.go
+++ main.go
@@ -1,12 +1,23 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"os"
 	"ts-game/engine"
 )
 
 func main() {
 	server := engine.New()
-	server.Connect(os.Stdin, os.Stdout)
+	if len(os.Args) == 1 {
+		fmt.Println("No mode provided, defaulting to local")
+		server.Connect(os.Stdin, os.Stdout)
+	} else if os.Args[1] == "local" {
+		server.Connect(os.Stdin, os.Stdout)
+	} else if os.Args[1] == "telnet" {
+		fmt.Println("TODO: Handle telnet")
+	} else {
+		log.Fatalf("Unknown mode: %v", os.Args[1])
+	}
 	server.Start()
 }
