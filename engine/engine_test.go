5036e969d72ab48a02a5b68a24e74d1a1544d1c2
Justin Thurman <justin.thurman@bevy.com>
2024-07-02 07:02:33 -0400

    Fix unit test to wait for room loading

--- engine/engine_test.go
+++ engine/engine_test.go
@@ -5,20 +5,38 @@ import (
 	"bytes"
 	"strings"
 	"testing"
+	"time"
+	"ts-game/room"
 )
 
 func TestServerRuns(t *testing.T) {
 	server := New()
-	go server.Start()
+	go func() {
+		err := server.Start()
+		if err != nil {
+			t.Errorf(err.Error())
+		}
+	}()
 	input := "TestPlayer\n"
 	r := strings.NewReader(input)
 	var output bytes.Buffer
 	w := bufio.NewWriter(&output)
+	for len(room.Rooms) <= 0 {
+		time.Sleep(time.Millisecond * 10)
+	}
 	server.Connect(r, w, func() {})
 
 	w.Flush()
 
 	expected := "Welcome to my very professional game, TestPlayer!"
+	if !strings.Contains(output.String(), expected) {
+		t.Errorf("expected %q to be in output %q", expected, output.String())
+	}
+	expected = "Town Center"
+	if !strings.Contains(output.String(), expected) {
+		t.Errorf("expected %q to be in output %q", expected, output.String())
+	}
+	expected = "You are standing in the bustling town center."
 	if !strings.Contains(output.String(), expected) {
 		t.Errorf("expected %q to be in output %q", expected, output.String())
 	}
