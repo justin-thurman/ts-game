6e80e757437d9ec87f50fc77b21214942655e64f
Justin Thurman <justin.thurman@bevy.com>
2024-07-12 10:33:50 -0400

    Refactor to use pgxpool

--- persistence/db.go
+++ persistence/db.go
@@ -5,7 +5,7 @@ import (
 	"log"
 	"os"
 
-	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 func Test() {
@@ -13,14 +13,14 @@ func Test() {
 	if dbUrl == "" {
 		log.Fatal("DATABASE_URL env var not set")
 	}
-	conn, err := pgx.Connect(context.Background(), dbUrl)
+	dbpool, err := pgxpool.New(context.Background(), dbUrl)
 	if err != nil {
 		log.Fatalf("Unable to connect to database: %v\n", err)
 	}
-	defer conn.Close(context.Background())
+	defer dbpool.Close()
 
 	var greeting string
-	err = conn.QueryRow(context.Background(), "SELECT 'Hello, world!'").Scan(&greeting)
+	err = dbpool.QueryRow(context.Background(), "SELECT 'Hello, world!'").Scan(&greeting)
 	if err != nil {
 		log.Fatalf("QueryRow failed: %v\n", err)
 	}
